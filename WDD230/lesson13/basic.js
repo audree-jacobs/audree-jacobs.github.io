cd39c3d32b45052c1242511be6ae249776adfec4
audree-jacobs <jac17003@byui.edu>
2021-12-11 19:05:08 -0700

    fixed toggle

--- WDD230/lesson13/basic.js
+++ WDD230/lesson13/basic.js
@@ -1,9 +1,12 @@
 
 
 
+document.getElementById("mobile_nav").style.display = "none";
+
+
 function mobile() {
     var x = document.getElementById("mobile_nav");
-    if (x.style.display === "none") {
+    if (x.style.display == "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
