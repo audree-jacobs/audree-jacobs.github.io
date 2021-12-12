448cabd1a82483c1ce257e11d09749082445468a
audree-jacobs <jac17003@byui.edu>
2021-12-11 18:55:41 -0700

    more taylor swift work

---
+++ WDD230/lesson13/basic.js
@@ -0,0 +1,11 @@
+
+
+
+function mobile() {
+    var x = document.getElementById("mobile_nav");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
