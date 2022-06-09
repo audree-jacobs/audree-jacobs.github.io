40d0840f4230c74be35ba6a0b9ce7cc2ae2a2468
Audrey Jacobs <71578039+audree-jacobs@users.noreply.github.com>
2022-06-09 08:46:44 -0600

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,23 @@
+
+console.log("hi"); 
+
+
+
+// document.querySelector(“.green”).addEventListener(“click”, fucntionName()); 
+// functionName {
+// Classlist.add = animate; 
+// } 
+
+
+function animateLetter() {
+    let flap = document.querySelector('.flap'); 
+    let heart = document.querySelector('.heart');
+    flap.classList.add("animate");
+    heart.classList.add("animate2");
+    let note = document.querySelector('.note');
+    note.classList.add("appear");
+
+    console.log("clicked!"); 
+}; 
+
+
